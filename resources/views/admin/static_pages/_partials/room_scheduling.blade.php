4f6bbdf341f7f205553bb1f058352ebb6c82bfed
Aswin TS <aswin@spiderworks.in>
2024-10-25 15:38:53 +0530

    room_scheduling3

--- resources/views/admin/static_pages/_partials/room_scheduling.blade.php
+++ resources/views/admin/static_pages/_partials/room_scheduling.blade.php
@@ -56,13 +56,26 @@
 </div>
 <div class="form-group col-md-12">
     <label>Short Description top </label>
-    <input type="text" name="content[banner_title]" class="form-control"
+    <input type="text" name="content[banner_title2]" class="form-control"
         @if ($obj->content && isset($obj->content['banner_title2'])) value="{{ $obj->content['banner_title2'] }}" @endif>
 </div>
 
-<div style="display:none" id="video-clone-holder">
-    @include('admin.media.set_file', ['file'=>null, 'title'=>'Gallery Video', 'popup_type'=>'single_image', 'type'=>'Video', 'holder_attr'=>'gallery_medias[]', 'id'=>'video_holder'])
+<div class="form-group ">
+    @php
+    $media_id_works_video_thumb_img = ($obj->content && isset($obj->content['media_id_works_video_thumb_img']))?$obj->content['media_id_works_video_thumb_img']:null;
+    @endphp
+    @include('admin.media.set_file', ['file'=>$media_id_works_video_thumb_img, 'title'=>'Video Thumb  Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_works_video_thumb_img]', 'id'=>'media_id_works_video_thumb_img', 'display'=> 'horizontal'])
 </div>
+<div class="form-group">
+    @php
+    $media_id_works_video= ($obj->content && isset($obj->content['media_id_works_video']))?$obj->content['media_id_works_video']:null;
+    @endphp
+    @include('admin.media.set_file', ['file'=>$media_id_works_video, 'title'=>'Works Video', 'popup_type'=>'single_image', 'type'=>'Video', 'holder_attr'=>'content[media_id_works_video]', 'id'=>'media_id_works_video', 'display'=> 'horizontal'])
+    <div class="form-group col-md-12">
+    <textarea class="form-control" name="content[youtube_video_link]">@if ($obj->content && isset($obj->content['youtube_video_link'])){{$obj->content['youtube_video_link']}}@endif
+    </textarea>
+
+    </div>
 
 
 
