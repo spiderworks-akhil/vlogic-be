dbbdb7e0741d6329c27f542b31b491303ff5810a
Aswin TS <aswin@spiderworks.in>
2024-10-25 15:30:07 +0530

    room_scheduling2

--- resources/views/admin/static_pages/_partials/room_scheduling.blade.php
+++ resources/views/admin/static_pages/_partials/room_scheduling.blade.php
@@ -20,5 +20,56 @@
         </div>
 
     </fieldset>
+
+<h3>Bottom content</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Heading</label>
+        <input type="text" name="content[works_heading1]" class="form-control"
+            @if ($obj->content && isset($obj->content['works_heading1'])) value="{{ $obj->content['works_heading1'] }}" @endif>
+    </div>
+    <div class="form-group col-md-12">
+        <label>Short Description top </label>
+        <input type="text" name="content[banner_title1]" class="form-control"
+            @if ($obj->content && isset($obj->content['banner_title1'])) value="{{ $obj->content['banner_title1'] }}" @endif>
+    </div>
+    @php
+    $media_id_works_first_featured_image =
+        $obj->content && isset($obj->content['media_id_works_first_featured_image'])
+            ? $obj->content['media_id_works_first_featured_image']
+            : null;
+@endphp
+@include('admin.media.set_file', [
+    'file' => $media_id_works_first_featured_image,
+    'title' => 'Banner Image ',
+    'popup_type' => 'single_image',
+    'type' => 'Image',
+    'holder_attr' => 'content[media_id_works_first_featured_image]',
+    'id' => 'media_id_works_first_featured_image',
+    'display' => 'horizontal',
+])
+
+<div class="form-group col-md-12">
+    <label>Heading</label>
+    <input type="text" name="content[works_heading2]" class="form-control"
+        @if ($obj->content && isset($obj->content['works_heading2'])) value="{{ $obj->content['works_heading2'] }}" @endif>
+</div>
+<div class="form-group col-md-12">
+    <label>Short Description top </label>
+    <input type="text" name="content[banner_title]" class="form-control"
+        @if ($obj->content && isset($obj->content['banner_title2'])) value="{{ $obj->content['banner_title2'] }}" @endif>
+</div>
+
+<div style="display:none" id="video-clone-holder">
+    @include('admin.media.set_file', ['file'=>null, 'title'=>'Gallery Video', 'popup_type'=>'single_image', 'type'=>'Video', 'holder_attr'=>'gallery_medias[]', 'id'=>'video_holder'])
+</div>
+
+
+
+
+</fieldset>
+
+
+
 </div>
 
