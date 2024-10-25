146af0cffae416646f4a35a231a5478219cb89e1
Aswin TS <aswin@spiderworks.in>
2024-10-25 11:27:50 +0530

    videos

--- app/Http/Controllers/Apis/WebinarController.php
+++ app/Http/Controllers/Apis/WebinarController.php
@@ -1,27 +1,28 @@
 <?php
 
-namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
 namespace App\Http\Controllers\Apis;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\Media;
-use App\Http\Resources\MediaCollection;
 use App\Models\Webinar;
+
+use App\Models\FrontendPage;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\WebinarResourceCollection;
 
 class WebinarController extends Controller
 {
     public function webinar (){
+
+        $banner = FrontendPage::where('slug','webinar')->get();
+
+
         $data = Webinar::with('media')->get();
         if (is_null($data)) {
             return response()->json(['message' => 'Government page not found'], 400);
         }
 
 
-        return new MediaCollection($data);
+        return new WebinarResourceCollection($data,$banner );
 
 
     }
---
+++ app/Http/Resources/WebinarResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WebinarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+
+            'id'=>$this->id,
+            'slug'=>$this->slug,
+            'title'=>$this->title,
+            'content'=>$this->content,
+            'browser_title'=>$this->browser_title,
+            'banner_image' => new Media($this->media),
+        ];
+    }
+}
