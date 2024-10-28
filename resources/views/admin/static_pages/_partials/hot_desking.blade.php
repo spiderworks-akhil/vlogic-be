aa7a7f5ad83f1e2843860de5c0cbbaa94fed2b26
Aswin TS <aswin@spiderworks.in>
2024-10-28 09:52:08 +0530

    gov

---
+++ resources/views/admin/static_pages/_partials/hot_desking.blade.php
@@ -0,0 +1,50 @@
+<div id="form-vertical" class="form-horizontal form-wizard-wrapper">
+    <h3>Banner Section</h3>
+        <fieldset>
+
+        <div class="form-group col-md-12">
+            <label>Banners title</label>
+            <input type="text" name="content[banner_title]" class="form-control" @if($obj->content && isset($obj->content['banner_title'])) value="{{$obj->content['banner_title']}}" @endif >
+        </div>
+
+        <div class="form-group ">
+            @php
+            $media_id_banner_image = ($obj->content && isset($obj->content['media_id_banner_image']))?$obj->content['media_id_banner_image']:null;
+            @endphp
+            @include('admin.media.set_file', ['file'=>$media_id_banner_image, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_banner_image]', 'id'=>'media_id_banner_image', 'display'=> 'horizontal'])
+        </div>
+
+        <div class="form-group col-md-12">
+            <label>Banner Short Description</label>
+            <textarea name="content[banner_shortdescription]" class="form-control ">@if($obj->content && isset($obj->content['banner_shortdescription'])) {{$obj->content['banner_shortdescription']}} @endif</textarea>
+        </div>
+
+    </fieldset>
+
+
+
+
+
+
+    <h3>Bottom content</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Heading</label>
+        <input type="text" name="content[works_heading1]" class="form-control"
+            @if ($obj->content && isset($obj->content['works_heading1'])) value="{{ $obj->content['works_heading1'] }}" @endif>
+    </div>
+
+    <div class="card-body row">
+        <div class="form-group col-md-12">
+            <label>Challenges</label>
+            <textarea name="content[section_description_first]" class="form-control editor ">
+    @if ($obj->content && isset($obj->content['section_description_first']))
+    {{ $obj->content['section_description_first'] }}
+    @endif
+    </textarea>
+        </div>
+    </div>
+
+
+</fieldset>
+    </div>
