aad054720695e628e2c1db57bb00f1afb8e0e948
Aswin TS <aswin@spiderworks.in>
2024-11-05 14:57:07 +0530

    hot desking

--- resources/views/admin/static_pages/_partials/space_management.blade.php
+++ resources/views/admin/static_pages/_partials/space_management.blade.php
@@ -52,9 +52,9 @@
       <div class="card-body row">
         <div class="form-group col-md-12">
             <label>Occupancy in Real-time </label>
-            <textarea name="content[section_description_first01]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first01']))
-{{ $obj->content['section_description_first01'] }}
+            <textarea name="content[section_description_first001]" class="form-control editor ">
+@if ($obj->content && isset($obj->content['section_description_first001']))
+{{ $obj->content['section_description_first001'] }}
 @endif
 </textarea>
         </div>
