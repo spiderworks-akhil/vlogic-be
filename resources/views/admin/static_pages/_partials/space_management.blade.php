f9c8b196bd0c28f8260eea1c62b5aaeb4e98ba6b
Aswin TS <aswin@spiderworks.in>
2024-11-05 15:08:52 +0530

    hot desking

--- resources/views/admin/static_pages/_partials/space_management.blade.php
+++ resources/views/admin/static_pages/_partials/space_management.blade.php
@@ -31,126 +31,12 @@
 
 <h3>Work section </h3>
 <fieldset>
-
     <div class="form-group col-md-12">
         <label>Head Content</label>
         <input type="text" name="content[banner_title0]" class="form-control"
             @if($obj->content && isset($obj->content['banner_title0'])) value="{{ $obj->content['banner_title0'] }}" @endif>
     </div>
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Speed-up </label>
-    <textarea name="content[section_description_first0]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first0']))
-{{ $obj->content['section_description_first0'] }}
-@endif
-</textarea>
-</div>
-</div>
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Occupancy in Real-time </label>
-    <textarea name="content[section_description_first001]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first001']))
-{{ $obj->content['section_description_first001'] }}
-@endif
-</textarea>
-</div>
-</div>
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Manage Occupant </label>
-    <textarea name="content[section_description_first02]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first02']))
-{{ $obj->content['section_description_first02'] }}
-@endif
-</textarea>
-</div>
-</div>
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Room Assignments</label>
-    <textarea name="content[section_description_first03]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first03']))
-{{ $obj->content['section_description_first03'] }}
-@endif
-</textarea>
-</div>
-</div>
-
-
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Take Control</label>
-    <textarea name="content[section_description_first04]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first04']))
-{{ $obj->content['section_description_first04'] }}
-@endif
-</textarea>
-</div>
-</div>
-
-<div class="card-body row">
-<div class="form-group col-md-12">
-    <label>Manage Furniture</label>
-    <textarea name="content[section_description_first06]" class="form-control editor ">
-@if ($obj->content && isset($obj->content['section_description_first06']))
-{{ $obj->content['section_description_first06'] }}
-@endif
-</textarea>
-</div>
-</div>
-    @php
-        $featuredImage = $obj->content['media_id_works_first_featured_image' . $index] ?? null;
-    @endphp
-    @include('admin.media.set_file', [
-        'file' => $featuredImage,
-        'title' => 'Works Featured Image ' . $index,
-        'popup_type' => 'single_image',
-        'type' => 'Image',
-        'holder_attr' => 'content[media_id_works_first_featured_image' . $index . ']',
-        'id' => 'media_id_works_first_featured_image' . $index,
-        'display' => 'horizontal',
-    ])
-
 </fieldset>
-
-<!-- Middle Section -->
-<h3>Middle Section</h3>
-<fieldset>
-
-    <div class="form-group col-md-12">
-        <label>Main Heading</label>
-        <input type="text" name="content[works_heading_{{ $index }}]" class="form-control"
-            @if($obj->content && isset($obj->content['works_heading_' . $index]))
-                value="{{ $obj->content['works_heading_' . $index] }}"
-            @endif>
-    </div>
-
-    <div class="form-group col-md-12">
-        <label>Short Description</label>
-        <textarea name="content[works_shortdescription_{{ $index }}]" class="form-control">
-@if($obj->content && isset($obj->content['works_shortdescription_' . $index]))
-{{ $obj->content['works_shortdescription_' . $index] }}
-@endif
-</textarea>
-    </div>
-
-    @php
-        $featuredImage  = $obj->content['media_id_works_first_featured_image' . $index] ?? null;
-    @endphp
-    @include('admin.media.set_file', [
-        'file' => $featuredImage,
-        'title' => 'Works Featured Image ' . $index,
-        'popup_type' => 'single_image',
-        'type' => 'Image',
-        'holder_attr' => 'content[media_id_works_first_featured_image' . $index . ']',
-        'id' => 'media_id_works_first_featured_image' . $index,
-        'display' => 'horizontal',
-    ])
-
-</fieldset>
-
 
 
 </div>
