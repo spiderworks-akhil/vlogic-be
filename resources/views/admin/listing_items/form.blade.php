36d4c88d268518d0397f155bf2f54755ff5e6169
Aswin TS <aswin@spiderworks.in>
2024-11-05 16:03:33 +0530

    final

--- resources/views/admin/listing_items/form.blade.php
+++ resources/views/admin/listing_items/form.blade.php
@@ -149,31 +149,34 @@
                                                         </div>
 
                                                     </div>
+
+
+                                                    <div class="card-body row">
+                                                        <div class="form-group col-md-12">
+                                                            <label>Challenge </label>
+                                                            <textarea name="content[section_description_first0]" class="form-control editor ">
+                                                @if ($obj->content && isset($obj->content['section_description_first0']))
+                                                {{ $obj->content['section_description_first0'] }}
+                                                @endif
+                                                </textarea>
+                                                        </div>
+                                                    </div>
+
+
+                                                    <div class="card-body row">
+                                                        <div class="form-group col-md-12">
+                                                            <label>solution </label>
+                                                            <textarea name="content[section_description_first01]" class="form-control editor ">
+                                                @if ($obj->content && isset($obj->content['section_description_first01']))
+                                                {{ $obj->content['section_description_first01'] }}
+                                                @endif
+                                                </textarea>
+                                                        </div>
+                                                    </div>
+
                                                 </div><!--end card-body-->
                                             </div><!--end card-->
 
-                                            <div class="card-body row">
-                                                <div class="form-group col-md-12">
-                                                    <label>Challenge </label>
-                                                    <textarea name="content[section_description_first0]" class="form-control editor ">
-                                        @if ($obj->content && isset($obj->content['section_description_first0']))
-                                        {{ $obj->content['section_description_first0'] }}
-                                        @endif
-                                        </textarea>
-                                                </div>
-                                            </div>
-
-
-                                            <div class="card-body row">
-                                                <div class="form-group col-md-12">
-                                                    <label>solution </label>
-                                                    <textarea name="content[section_description_first01]" class="form-control editor ">
-                                        @if ($obj->content && isset($obj->content['section_description_first01']))
-                                        {{ $obj->content['section_description_first01'] }}
-                                        @endif
-                                        </textarea>
-                                                </div>
-                                            </div>
 
 
 
