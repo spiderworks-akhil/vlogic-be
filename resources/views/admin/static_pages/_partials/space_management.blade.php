14ca67aee97db2ce535185d423165fe2f7828f17
Aswin TS <aswin@spiderworks.in>
2024-11-05 15:11:35 +0530

    final

--- resources/views/admin/static_pages/_partials/space_management.blade.php
+++ resources/views/admin/static_pages/_partials/space_management.blade.php
@@ -36,6 +36,28 @@
         <input type="text" name="content[banner_title1]" class="form-control"
             @if($obj->content && isset($obj->content['banner_title1'])) value="{{ $obj->content['banner_title1'] }}" @endif>
     </div>
+
+
+    <div class="card-body row">
+        <div class="form-group col-md-12">
+            <label>Speed-up </label>
+            <textarea name="content[section_description_first0]" class="form-control editor ">
+@if ($obj->content && isset($obj->content['section_description_first0']))
+{{ $obj->content['section_description_first0'] }}
+@endif
+</textarea>
+        </div>
+    </div>
+    <div class="card-body row">
+        <div class="form-group col-md-12">
+            <label> Real-time </label>
+            <textarea name="content[section_description_first01]" class="form-control editor ">
+@if ($obj->content && isset($obj->content['section_description_first01']))
+{{ $obj->content['section_description_first01'] }}
+@endif
+</textarea>
+        </div>
+    </div>
 </fieldset>
 
 
