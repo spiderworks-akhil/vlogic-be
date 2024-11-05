834589c6d19145b0afbdd4ebc7fd67684218d9b9
Aswin TS <aswin@spiderworks.in>
2024-11-05 15:27:55 +0530

    final

--- resources/views/admin/static_pages/_partials/room_scheduling.blade.php
+++ resources/views/admin/static_pages/_partials/room_scheduling.blade.php
@@ -48,45 +48,6 @@
 
 
 
-    @php
-    $media_id_works_first_featured_image =
-        $obj->content && isset($obj->content['media_id_works_first_featured_image'])
-            ? $obj->content['media_id_works_first_featured_image']
-            : null;
-@endphp
-@include('admin.media.set_file', [
-    'file' => $media_id_works_first_featured_image,
-    'title' => 'Banner Image ',
-    'popup_type' => 'single_image',
-    'type' => 'Image',
-    'holder_attr' => 'content[media_id_works_first_featured_image]',
-    'id' => 'media_id_works_first_featured_image',
-    'display' => 'horizontal',
-])
-
-<div class="form-group col-md-12">
-    <label>Heading</label>
-    <input type="text" name="content[works_heading2]" class="form-control"
-        @if ($obj->content && isset($obj->content['works_heading2'])) value="{{ $obj->content['works_heading2'] }}" @endif>
-</div>
-
-
-<div class="form-group ">
-    @php
-    $media_id_works_video_thumb_img = ($obj->content && isset($obj->content['media_id_works_video_thumb_img']))?$obj->content['media_id_works_video_thumb_img']:null;
-    @endphp
-    @include('admin.media.set_file', ['file'=>$media_id_works_video_thumb_img, 'title'=>'Video Thumb  Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_works_video_thumb_img]', 'id'=>'media_id_works_video_thumb_img', 'display'=> 'horizontal'])
-</div>
-<div class="form-group">
-    @php
-    $media_id_works_video= ($obj->content && isset($obj->content['media_id_works_video']))?$obj->content['media_id_works_video']:null;
-    @endphp
-    @include('admin.media.set_file', ['file'=>$media_id_works_video, 'title'=>'Works Video', 'popup_type'=>'single_image', 'type'=>'Video', 'holder_attr'=>'content[media_id_works_video]', 'id'=>'media_id_works_video', 'display'=> 'horizontal'])
-    <div class="form-group col-md-12">
-    <textarea class="form-control" name="content[youtube_video_link]">@if ($obj->content && isset($obj->content['youtube_video_link'])){{$obj->content['youtube_video_link']}}@endif
-    </textarea>
-
-    </div>
 
 
 
