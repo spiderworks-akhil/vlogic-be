14565a2150d04cdeda57abbeae5a4ddc2b3cdd04
Aswin TS <aswin@spiderworks.in>
2024-11-08 14:23:22 +0530

    final

--- app/Http/Controllers/Apis/WebinarController.php
+++ app/Http/Controllers/Apis/WebinarController.php
@@ -31,20 +31,23 @@ class WebinarController extends Controller
 
 
     public function detail(Request $request, $slug)
-    {
+{
+    try {
+        $webinar_detail = Webinar::where('slug', $slug)
+            ->where('status', 1)
+            ->first();
 
-        try {
-            $webinar_detail = Webinar::where('slug', $slug)
-                ->where('status', 1)
-                ->first();
-
-            if (!$webinar_detail) {
-                return response()->json(['error' => 'Not found'], 404);
-            }
-            $webinar_detail = new WebinarResource($webinar_detail);
-            return response()->json($webinar_detail);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
+        if (!$webinar_detail) {
+            return response()->json(['error' => 'Not found'], 404);
         }
+
+        $webinar_detail = new WebinarResource($webinar_detail);
+
+        // Return the response inside a 'data' key
+        return response()->json(['data' => $webinar_detail]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
     }
+}
+
 }
