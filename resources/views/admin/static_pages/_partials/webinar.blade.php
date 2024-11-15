76dadd91e555b9ffe5dd4eb6172a1f4d38690c4f
Aswin TS <aswin@spiderworks.in>
2024-11-15 17:10:12 +0530

    space management list

--- resources/views/admin/static_pages/_partials/webinar.blade.php
+++ resources/views/admin/static_pages/_partials/webinar.blade.php
@@ -13,9 +13,12 @@
             @endphp
             @include('admin.media.set_file', ['file'=>$media_id_banner_image, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_banner_image]', 'id'=>'media_id_banner_image', 'display'=> 'horizontal'])
         </div>
-
         <div class="form-group col-md-12">
-            <label>Banner Short Description</label>
+            <label>Title</label>
+            <input type="text" name="content[banner_title1]" class="form-control" @if($obj->content && isset($obj->content['banner_title1'])) value="{{$obj->content['banner_title1']}}" @endif >
+        </div>
+        <div class="form-group col-md-12">
+            <label>Short Description</label>
             <textarea name="content[banner_shortdescription]" class="form-control ">@if($obj->content && isset($obj->content['banner_shortdescription'])) {{$obj->content['banner_shortdescription']}} @endif</textarea>
         </div>
 
