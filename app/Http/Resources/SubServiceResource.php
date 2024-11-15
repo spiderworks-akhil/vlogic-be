347679a85d7b81c322f3bd5d1d0ca5a360f3573a
Aswin TS <aswin@spiderworks.in>
2024-11-15 15:21:30 +0530

    space management list

---
+++ app/Http/Resources/SubServiceResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => new FrontendPageContentResource($this->content),
+            'url' => $this->url,
+            'icon_image' => new Media($this->icon_image),
+            'featured_image' => new Media($this->featured_image),
+            'banner_image' => new Media($this->banner_image),
+            'browser_title' => $this->browser_title,
+            'og_title' => $this->og_title,
+            'meta_description' => $this->meta_description,
+            'og_description' => $this->og_description,
+            'og_image' => new Media($this->og_image),
+            'meta_keywords' => $this->meta_keywords,
+            'bottom_description' => $this->bottom_description,
+            'extra_js' => $this->extra_js,
+            'faq' => new FaqCollection($this->faq),
+        //    'gallery' => new MediaCollection($this->gallery),
+            'children' => new ServiceCollection($this->whenLoaded('children')),
+        
+
+        ];
+    }
+}
