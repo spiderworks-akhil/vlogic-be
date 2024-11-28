bb9838df940591a8914de934735a883b172bdadd
Aswin TS <aswin@spiderworks.in>
2024-11-28 15:11:48 +0530

    form

---
+++ resources/views/admin/static_pages/_partials/vlogicfm.blade.php
@@ -0,0 +1,51 @@
+
+<div id="form-vertical" class="form-horizontal form-wizard-wrapper">
+    <!-- Head Section -->
+    <h3>Banner Section</h3>
+    <fieldset>
+        <div class="form-group col-md-12">
+            <label>Banner Top Title</label>
+            <input type="text" name="content[banner_top_title]" class="form-control"
+                @if ($obj->content && isset($obj->content['banner_top_title'])) value="{{ $obj->content['banner_top_title'] }}" @endif>
+        </div>
+        <div class="form-group col-md-12">
+            <label>Banner title</label>
+            <input type="text" name="content[banner_title]" class="form-control"
+                @if ($obj->content && isset($obj->content['banner_title'])) value="{{ $obj->content['banner_title'] }}" @endif>
+        </div>
+        <div class="form-group col-md-12">
+            <label>Banner Short Description</label>
+            <input type="text" name="content[banner_description]" class="form-control"
+                @if ($obj->content && isset($obj->content['banner_description'])) value="{{ $obj->content['banner_description'] }}" @endif>
+        </div>
+
+
+        <div class="card-body row">
+            <div class="form-group col-md-12">
+                <label>Top Description </label>
+                <textarea name="content[banner_title2]" class="form-control editor ">
+    @if ($obj->content && isset($obj->content['banner_title2']))
+{{ $obj->content['banner_title2'] }}
+@endif
+    </textarea>
+            </div>
+        </div>
+
+
+
+
+        @php
+            $bannerImage = $obj->content['media_id_works_first_featured_image'] ?? null;
+        @endphp
+        @include('admin.media.set_file', [
+            'file' => $bannerImage,
+            'title' => 'Banner Image - ( width-100px x height-100px )',
+            'popup_type' => 'single_image',
+            'type' => 'Image',
+            'holder_attr' => 'content[media_id_works_first_featured_image]',
+            'id' => 'media_id_works_first_featured_image',
+            'display' => 'horizontal',
+        ])
+      
+    </fieldset>
+</div>
