d5fe38e51308317b4609bc371dda691da7aadf45
seethalSpiderWorks <seethalbabu213@gmail.com>
2024-12-05 10:44:51 +0530

    banner title change

--- resources/views/admin/static_pages/_partials/blogs.blade.php
+++ resources/views/admin/static_pages/_partials/blogs.blade.php
@@ -4,15 +4,17 @@
 
         <div class="form-group col-md-12">
             <label>Banner title</label>
-            <textarea name="content[banner_title]" class="form-control editor">@if($obj->content && isset($obj->content['banner_title'])) {{$obj->content['banner_title']}} @endif</textarea>
+            <input type="text" name="content[banner_title]" class="form-control" @if($obj->content && isset($obj->content['banner_title'])) value="{{$obj->content['banner_title']}}" @endif >
         </div>
 
         <div class="form-group">
             @php
             $media_id_banner_image = ($obj->content && isset($obj->content['media_id_banner_image']))?$obj->content['media_id_banner_image']:null;
             @endphp
-            @include('admin.media.set_file', ['file'=>$media_id_banner_image, 'title'=>'Banner Image - (width-800px x height-800px)opup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_banner_image]', 'id'=>'media_id_banner_image', 'display'=> 'horizontal'])
+            @include('admin.media.set_file', ['file'=>$media_id_banner_image, 'title'=>'Banner Image - (width-800px x height-800px)', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[media_id_banner_image]', 'id'=>'media_id_banner_image', 'display'=> 'horizontal'])
         </div>
+
+
 
         <div class="form-group col-md-12">
             <label>Banner Short Description</label>
