3fd1083e3695dfc2b649ed8470bfa3f4f9b02fa8
seethalSpiderWorks <seethalbabu213@gmail.com>
2024-12-06 10:58:08 +0530

    brochure mail bug fixing

--- app/Http/Requests/BrochureRequest.php
+++ app/Http/Requests/BrochureRequest.php
@@ -26,22 +26,15 @@ class BrochureRequest extends FormRequest
             'name' => 'required|max:255',
             'phone_number' => 'required|max:20',
             'email' => ['nullable', 'email', 'max:255'],
-            'organization' => ['required'],
-            'country' => ['required'],
-            'message' => ['nullable', new Spam()],
         ];
     }
 
     public function messages()
     {
-        return [
+        return[
             'name.required' => 'Please enter your first name',
             'phone_number.required' => 'Please enter your phone number',
             'email.email' => 'Please enter a valid email address',
-            'phone_number.required' => 'Please enter your phone number',
-            'organization.reruired' =>'Please enter your organization name',
-            'country.required' => 'please enter your country name',
-
         ];
     }
 }
