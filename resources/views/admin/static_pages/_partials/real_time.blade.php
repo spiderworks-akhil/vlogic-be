f79159f69c86bef5d11260c5a92e03f186f22432
seethalSpiderWorks <seethalbabu213@gmail.com>
2024-12-11 16:55:37 +0530

    changes

--- resources/views/admin/static_pages/_partials/real_time.blade.php
+++ resources/views/admin/static_pages/_partials/real_time.blade.php
@@ -20,8 +20,8 @@
         </div>
 
         <div class="form-group col-md-12">
-            <label>Short Description</label>
-            <textarea name="content[banner_shortdescription0]" class="form-control editor">@if($obj->content && isset($obj->content['banner_shortdescription0'])) {{$obj->content['banner_shortdescription0']}} @endif</textarea>
+            <label>Banner Description</label>
+            <textarea name="content[banner_description]" class="form-control editor">@if ($obj->content && isset($obj->content['banner_description'])) {{$obj->content['banner_description']}} @endif</textarea>
         </div>
 
         <div class="form-group col-md-12">
